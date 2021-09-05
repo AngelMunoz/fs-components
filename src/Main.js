c9902452fd5d4490d0f30d02509f748bd3134539
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-05 15:39:53 -0500

    correct exports and

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 export { register as registerMessage } from './Message.fs.js';
 export { register as registerOffCanvas } from './OffCanvas.fs.js';
-export * from './Tabs.fs';
+export { TabItem, registerTabHost, registerTabItem } from './Tabs.fs.js';
 export * from './Library.fs.js';
 
 // use named exports when possible to let your users use tree-shake if they want.
