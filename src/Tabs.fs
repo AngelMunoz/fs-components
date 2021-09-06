18a25a91f80f7e022e025b84eea4d69d1b1f99fd
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-06 02:18:22 +0000

    make tabs work

--- src/Tabs.fs
+++ src/Tabs.fs
@@ -5,46 +5,31 @@ module Tabs
 open Fable.Core
 open Lit
 open Haunted
+open Fable.Core.JsInterop
 open Browser.Types
 open ShadowStyles
 open Types
 open Styles
 
-[<AttachMembers>]
-type TabItem =
-    { id: string
-      label: string
-      closable: bool }
-
-    static member FromObject
-        (tabLike: {| id: string
-                     label: string option
-                     closable: bool option |})
-        : TabItem =
-        { id = tabLike.id
-          label = tabLike.label |> Option.defaultValue tabLike.id
-          closable = tabLike.closable |> Option.defaultValue false }
-
 [<AllowNullLiteral>]
 type FsTabItemElement =
     inherit HTMLElement
 
-    abstract member selected : bool option with get, set
+    abstract member isSelected : bool option with get, set
+    abstract member isClosable : bool option with get, set
     abstract member label : string option with get, set
-    abstract member closable : bool option with get, set
-    abstract member tabItem : TabItem option with get, set
+    abstract member tabName : string option with get, set
 
 
 let private initializeTabItem (host: FsTabItemElement) _ =
-    match host.tabItem with
+    match host.isSelected with
     | Some _ -> ()
-    | None ->
-        host.tabItem <-
-            {| id = host.id
-               label = host.label
-               closable = host.closable |}
-            |> TabItem.FromObject
-            |> Some
+    | None -> host.isSelected <- Some false
+
+    match host.isClosable with
+    | Some _ -> ()
+    | None -> host.isClosable <- Some false
+
 
 let private tabItemStyles = [ fsComponentsStyles; fsTabItemStyles ]
 
@@ -52,54 +37,46 @@ let private FsTabItem (host: FsTabItemElement) =
     ShadowStyles.adoptStyleSheets (host, tabItemStyles)
     Haunted.useEffect ((initializeTabItem host), [||])
 
+    let onClose (ev: MouseEvent) =
+        ev.preventDefault ()
+        ev.stopImmediatePropagation ()
+        ev.stopPropagation ()
 
-    let onClose (host: FsTabItemElement) _ =
         let ev =
             Haunted.createEvent (
                 "on-fs-tab-close",
-                {| composed = true
-                   bubbles = true
-                   cancelable = true |}
+                {| bubbles = true
+                   cancelable = true
+                   composed = true |}
             )
 
         host |> dispatchEvent ev
 
-    let onSelected (host: FsTabItemElement) _ =
+    let onSelected _ =
         let ev =
             Haunted.createEvent (
                 "on-fs-tab-selected",
-                {| composed = true
-                   bubbles = true
-                   cancelable = true |}
+                {| bubbles = true
+                   cancelable = true
+                   composed = true |}
             )
 
         host |> dispatchEvent ev
 
-    let labelTpl () =
-        match host.label with
-        | Some label -> html $"<span>{label}</span>"
-        | None ->
-            match host.tabItem with
-            | Some tabItem -> html $"<span>{tabItem.label}</span>"
-            | None -> html $"<span>{host.id}</span>"
-
-    let closeTpl () =
-        match host.closable with
-        | Some true -> html $"<button class='close' @click={onClose host}>&times;</button>"
-        | Some false -> Lit.nothing
-        | None ->
-            match host.tabItem with
-            | Some tabItem ->
-                match tabItem.closable with
-                | true -> html $"<button class='close' @click={onClose host}>&times;</button>"
-                | false -> Lit.nothing
-            | None -> Lit.nothing
+    let closableTpl () =
+        match host.isClosable with
+        | Some true -> html $"<button class='delete' @click={onClose}>&times;</button>"
+        | _ -> Lit.nothing
 
     html
         $"""
-        <section @click={onSelected host}>
-            {closeTpl ()}
-            {labelTpl ()}
+        <section @click={onSelected}>
+            <slot>
+                <span>{host.label}</span>
+            </slot>
+            <slot name="icon">
+                {closableTpl ()}
+            </slot>
         </section>
         """
 
@@ -108,68 +85,46 @@ let private FsTabItem (host: FsTabItemElement) =
 type FsTabHostElement =
     inherit HTMLElement
 
-    abstract member tabs : TabItem array option with get, set
-    abstract member selectedTab : TabItem option with get, set
-
-let private initializeTabHost (host: FsTabHostElement) _ =
-    match host.tabs with
-    | Some _ -> ()
-    | None -> host.tabs <- Some Array.empty<TabItem>
-
 let private tabHostStyles = [ fsComponentsStyles; fsTabHostStyles ]
 
 let private FsTabHost (host: FsTabHostElement) =
     ShadowStyles.adoptStyleSheets (host, tabHostStyles)
-    Haunted.useEffect ((initializeTabHost host), [||])
 
-    let onTabClose (ev: Event) =
-        match host.tabs with
-        | Some tabs ->
-            let tab = (ev.target :?> FsTabItemElement).tabItem
 
-            match tab with
-            | None -> ()
-            | Some tab ->
-                host.tabs <-
-                    tabs
-                    |> Array.filter (fun t -> t.id <> tab.id)
-                    |> Some
+    let onSelected (ev: Event) =
+        ev.preventDefault ()
+        ev.stopImmediatePropagation ()
+        ev.stopPropagation ()
 
-                ev.preventDefault ()
-                ev.stopPropagation ()
-                ev.stopImmediatePropagation ()
-        | None -> ()
+        let target = (ev.target :?> HTMLElement)
+        let tabs = target.parentElement.children
 
-    let setSelectedTab (host: FsTabHostElement) (ev: Event) =
-        let target = (ev.target :?> FsTabItemElement)
+        for i in 0 .. tabs.length - 1 do
+            tabs.item(i).removeAttribute ("is-selected")
 
-        host.selectedTab <-
-            TabItem.FromObject(target.tabItem |> unbox)
-            |> Some
+        target.setAttribute ("is-selected", "")
 
-    let tabsTpl (tabs: TabItem array) =
-        seq {
-            for tab in tabs do
-                let isSelected =
-                    match host.selectedTab with
-                    | None -> false
-                    | Some selected -> selected.id = tab.id
+        let ev =
+            Haunted.createCustomEvent (
+                "on-fs-tab-selected",
+                {| bubbles = true
+                   cancelable = true
+                   composed = true
+                   detail = Some target |}
+            )
 
-                html
-                    $"""
-                    <fs-tab-item selected={isSelected} .tabItem={tab} @on-fs-tab-selected={setSelectedTab host} @on-fs-tab-closed={onTabClose}></fs-tab-item>
-                    """
-        }
+        host |> dispatchEvent ev
+
 
     html
         $"""
         <article>
             <nav>
-                <slot name="tabs">
-                    {tabsTpl (host.tabs |> Option.defaultValue [||])}
-                </slot>
+                <slot @on-fs-tab-selected={onSelected} name="tabs"></slot>
             </nav>
-            <slot name="tab-content"></slot>
+            <section class="tab-content">
+                <slot></slot>
+            </section>
         </article>
         """
 
@@ -188,8 +143,8 @@ let registerTabItem () =
         (Haunted.Component(
             FsTabItemRef,
             {| observedAttributes =
-                   [| "selected"
-                      "closable"
-                      "id"
-                      "label" |] |}
+                   [| "is-selected"
+                      "is-closable"
+                      "label"
+                      "tab-name" |] |}
         ))
