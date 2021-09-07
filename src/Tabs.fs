df3357e87e3f245e19e3c8ff659c40263892be41
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-07 13:23:37 +0000

    add tabs

--- src/Tabs.fs
+++ src/Tabs.fs
@@ -19,6 +19,7 @@ type FsTabItemElement =
     abstract member isClosable : bool option with get, set
     abstract member label : string option with get, set
     abstract member tabName : string option with get, set
+    abstract member kind : Kind option with get, set
 
 
 let private initializeTabItem (host: FsTabItemElement) _ =
@@ -30,12 +31,42 @@ let private initializeTabItem (host: FsTabItemElement) _ =
     | Some _ -> ()
     | None -> host.isClosable <- Some false
 
+    match host.kind with
+    | Some _ -> ()
+    | None -> host.kind <- Some Default
 
 let private tabItemStyles = [ fsComponentsStyles; fsTabItemStyles ]
+
+let private getClass (kind: Kind option) =
+    seq {
+        (match kind with
+         | Some Primary -> "is-primary"
+         | Some Info -> "is-info"
+         | Some Link -> "is-link"
+         | Some Success -> "is-success"
+         | Some Warning -> "is-warning"
+         | Some Danger -> "is-danger"
+         | Some Default -> ""
+         | None -> ""),
+        true
+    }
+
 
 let private FsTabItem (host: FsTabItemElement) =
     ShadowStyles.adoptStyleSheets (host, tabItemStyles)
     Haunted.useEffect ((initializeTabItem host), [||])
+
+    Haunted.useEffect (
+        (fun _ ->
+            match host.kind with
+            | Some kind ->
+                host.setAttribute ("kind", "")
+                host.setAttribute ("kind", $"{kind}")
+            | None -> ()
+
+            ),
+        [| host.kind |]
+    )
 
     let onClose (ev: MouseEvent) =
         ev.preventDefault ()
@@ -85,11 +116,34 @@ let private FsTabItem (host: FsTabItemElement) =
 type FsTabHostElement =
     inherit HTMLElement
 
+    abstract kind : Kind option with get, set
+
 let private tabHostStyles = [ fsComponentsStyles; fsTabHostStyles ]
 
 let private FsTabHost (host: FsTabHostElement) =
     ShadowStyles.adoptStyleSheets (host, tabHostStyles)
 
+    Haunted.useEffect (
+        (fun _ ->
+            match host.kind with
+            | Some _ -> ()
+            | None -> host.kind <- Some Default),
+        [||]
+    )
+
+    Haunted.useEffect (
+        (fun _ ->
+            match host.kind with
+            | Some kind ->
+                host.setAttribute ("kind", "")
+                host.setAttribute ("kind", $"{kind}")
+            | None -> ()
+
+            ),
+        [| host.kind |]
+    )
+
+    JS.console.log (host.kind)
 
     let onSelected (ev: Event) =
         ev.preventDefault ()
@@ -119,10 +173,10 @@ let private FsTabHost (host: FsTabHostElement) =
     html
         $"""
         <article>
-            <nav>
+            <nav class={Lit.classes (getClass host.kind)}>
                 <slot @on-fs-tab-selected={onSelected} name="tabs"></slot>
             </nav>
-            <section class="tab-content">
+            <section class={Lit.classes (getClass host.kind)}>
                 <slot></slot>
             </section>
         </article>
@@ -135,7 +189,7 @@ let private FsTabHostRef: obj = jsNative
 let private FsTabItemRef: obj = jsNative
 
 let registerTabHost () =
-    defineComponent "fs-tab-host" (Haunted.Component(FsTabHostRef))
+    defineComponent "fs-tab-host" (Haunted.Component(FsTabHostRef, {| observedAttributes = [| "kind" |] |}))
 
 let registerTabItem () =
     defineComponent
@@ -145,6 +199,7 @@ let registerTabItem () =
             {| observedAttributes =
                    [| "is-selected"
                       "is-closable"
+                      "kind"
                       "label"
                       "tab-name" |] |}
         ))
