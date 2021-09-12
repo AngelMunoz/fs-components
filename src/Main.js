ea022025cab3853c036b7b645daa8e802593e465
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-12 01:16:33 +0000

    export styles as well

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 export { register as registerMessage } from './Message.fs.js';
 export { register as registerOffCanvas } from './OffCanvas.fs.js';
 export { registerTabHost, registerTabItem } from './Tabs.fs.js';
+export * from './Styles.fs.js';
 export * from './Library.fs.js';
 
 // use named exports when possible to let your users use tree-shake if they want.
